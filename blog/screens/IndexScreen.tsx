0c169c699dbeabd87e3d9816ca3d3b1631699d38
Sahil Maheshwari <sahilm1711@gmail.com>
2022-10-10 00:59:15 +0530

    adding useReducer

--- blog/screens/IndexScreen.tsx
+++ blog/screens/IndexScreen.tsx
@@ -1,10 +1,21 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, FlatList , Button} from 'react-native'
+import React,{useContext} from 'react'
+import BlogContext from '../context/BlogContext'
 
 const IndexScreen = () => {
+  const {data,addBlogPost}=useContext(BlogContext);
+
   return (
     <View>
       <Text>IndexScreen</Text>
+      <Button title="Add Post" onPress={addBlogPost}/>
+      <FlatList
+       data={data}
+       keyExtractor={blogPost=>blogPost.title}
+       renderItem={({item})=>{
+        return <Text>{item.title}</Text>
+       }}
+      />
     </View>
   )
 }
