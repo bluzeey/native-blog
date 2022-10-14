697cc9c36474802cee9ac04441e64cd4d65434b3
Sahil Maheshwari <sahilm1711@gmail.com>
2022-10-14 12:20:04 +0530

    adding changes to blog

--- blog/screens/IndexScreen.tsx
+++ blog/screens/IndexScreen.tsx
@@ -1,25 +1,56 @@
-import { StyleSheet, Text, View, FlatList , Button} from 'react-native'
-import React,{useContext} from 'react'
-import BlogContext from '../context/BlogContext'
+import React, { useContext } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  Button,
+  TouchableOpacity
+} from 'react-native';
+import { Context } from '../context/BlogContext';
+import { Feather } from '@expo/vector-icons';
 
 const IndexScreen = () => {
-  const {data,addBlogPost}=useContext(BlogContext);
+  const { state, addBlogPost, deleteBlogPost } = useContext(Context);
 
   return (
     <View>
-      <Text>IndexScreen</Text>
-      <Button title="Add Post" onPress={addBlogPost}/>
+      <Button title="Add Post" onPress={addBlogPost} />
       <FlatList
-       data={data}
-       keyExtractor={blogPost=>blogPost.title}
-       renderItem={({item})=>{
-        return <Text>{item.title}</Text>
-       }}
+        data={state}
+        keyExtractor={blogPost => blogPost.title}
+        renderItem={({ item }) => {
+          return (
+            <View style={styles.row}>
+              <Text style={styles.title}>
+                {item.title} - {item.id}
+              </Text>
+              <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                <Feather style={styles.icon} name="trash" />
+              </TouchableOpacity>
+            </View>
+          );
+        }}
       />
     </View>
-  )
-}
+  );
+};
 
-export default IndexScreen
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    borderTopWidth: 1,
+    borderColor: 'gray'
+  },
+  title: {
+    fontSize: 18
+  },
+  icon: {
+    fontSize: 24
+  }
+});
 
-const styles = StyleSheet.create({})
+export default IndexScreen;
